6447f3cfae517f5e32d77e00467fccbd857c8db2
Anna Adamik <quenthel5@gmail.com>
2020-11-15 08:40:26 +0100

    kot mruczy

--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,7 @@
 public class Cat {
     public static void main(String[] args) {
         System.out.println("Miauuu!!!");
+        System.out.println("Mrrrr");
+        System.out.println("Miauuu");
     }
 }
